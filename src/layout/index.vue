6ca130e1429350379adc4fc839a2e302db7249b5
mac <mac@macdeMac-mini.local>
2019-11-20 16:55:03 +0800

    修改

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-wrapper">
+  <div :class="classObj" class="app-wrapper">
     <!-- sidebar 侧边栏 -->
     <Sidebar class="sidebar-container" />
     <!-- 右侧内容区域 -->
@@ -15,22 +15,68 @@
 </template>
 <script>
 import { Navbar, Sidebar, TagsView } from "./components";
+import { mapState } from "vuex";
 export default {
   name: "Layout",
   components: {
     Navbar,
     Sidebar,
-    TagsView
-  }
+    TagsView,
+  },
+  computed: {
+    ...mapState({
+      sidebar: state => state.app.sidebar,
+    }),
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+      };
+    },
+  },
 };
 </script>
+
 <style lang="scss" scoped>
 @import "~@/styles/mixin.scss";
 @import "~@/styles/variables.scss";
+
 .app-wrapper {
   @include clearfix;
   position: relative;
-  width: 100%;
   height: 100%;
+  width: 100%;
+
+  &.mobile.openSidebar {
+    position: fixed;
+    top: 0;
+  }
+}
+
+.drawer-bg {
+  background: #000;
+  opacity: 0.3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+  transition: width 0.28s;
+}
+
+.hideSidebar .fixed-header {
+  width: calc(100% - 54px);
+}
+
+.mobile .fixed-header {
+  width: 100%;
 }
 </style>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -10,16 +10,23 @@ const mutations = {
   TOGGLE_SIDEBAR: state => {
     state.sidebar.opened = !state.sidebar.opened;
     if (state.sidebar.opened) {
-      Cookie.set("sidebarStatus", 0);
-    } else {
       Cookie.set("sidebarStatus", 1);
+    } else {
+      Cookie.set("sidebarStatus", 0);
     }
+  },
+  CLOSE_SIDEBAR: state => {
+    Cookie.set("sidebar", 0);
+    state.sidebar.opened = false;
   },
 };
 
 const actions = {
   toggleSideBar({ commit }) {
     commit("TOGGLE_SIDEBAR");
+  },
+  closeSideBar({ commit }) {
+    commit("CLOSE_SIDEBAR");
   },
 };
 
